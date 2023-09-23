981457a04e9741f28b78c09369c4b942e124bdab
Bashi <shtech@192.168.1.23>
2023-09-24 00:40:36 +0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,10 @@ export default function Home() {
       <Box component="section" className='grid md:grid-cols-2 h-[calc(100vh-179px)] md:!h-screen'>
         <Box component="div" className='flex flex-col order-1 justify-center items-center px-10 py-20 md:py-0'>
           <Box component="div" className='max-w-[500px]'>
-            <Typography variant="h6" className={`!text-4xl !text-center md:!text-left !font-bold text-[#F18753] font-moranga !leading-[3.2rem]`}> <Box component="span" className="bg-gradient-to-r to-[#F18753] via-[#F18753] from-[#EC2679] px-3 text-white">For</Box> Customers</Typography>
-            <Typography component="p" className='!text-2xl !text-center md:!text-left !mt-3'>Easy photo-based inventory of your belongings for an instant estimate and a quote you can trust</Typography>
-            <Typography variant="h6" className={`!text-4xl !mt-4 !text-center md:!text-left !font-bold text-[#F18753] font-moranga !leading-[3.2rem]`}> <Box component="span" className="bg-gradient-to-r to-[#F18753] via-[#F18753] from-[#EC2679] px-3 text-white">For Moving</Box> Companies</Typography>
-            <Typography component="p" className='!text-2xl !text-center md:!text-left !mt-3'>Better customer <br/>experience = higher <br/>conversions to bookings</Typography>
+            <Typography variant="h6" className={`text-2xl md:!text-4xl !text-center md:!text-left !font-bold text-[#F18753] font-moranga !leading-[3.2rem]`}> <Box component="span" className="bg-gradient-to-r to-[#F18753] via-[#F18753] from-[#EC2679] px-3 text-white">For</Box> Customers</Typography>
+            <Typography component="p" className='md:!text-2xl !text-center md:!text-left !mt-3'>Easy photo-based inventory of your belongings for an instant estimate and a quote you can trust</Typography>
+            <Typography variant="h6" className={`text-2xl md:!text-4xl !mt-4 !text-center md:!text-left !font-bold text-[#F18753] font-moranga !leading-[3.2rem]`}> <Box component="span" className="bg-gradient-to-r to-[#F18753] via-[#F18753] from-[#EC2679] px-3 text-white">For Moving</Box> Companies</Typography>
+            <Typography component="p" className='md:!text-2xl !text-center md:!text-left !mt-3'>Better customer <br/>experience = higher <br/>conversions to bookings</Typography>
           </Box>
         </Box>
         <Box component="div" className='relative group overflow-hidden'>
