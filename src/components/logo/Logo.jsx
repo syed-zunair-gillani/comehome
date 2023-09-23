c6c8573bc12282bb731f2738332054acada501e3
Bashi <shtech@192.168.1.23>
2023-09-23 23:46:04 +0500

    update

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Logo = () => {
   return (
     <Box component="figure" className='flex flex-col justify-center items-center'>
-          <Image src="/images/logo_t.png" alt="comehome logo" width={180} height={182} />
+          <Image src="/images/logo_t.png" alt="comehome logo" width={180} height={82} />
           <Typography component='p' className='flex justify-center !-mt-2 uppercase !text-sm rounded-md text-white px-1 !bg-orange-500'>Move Confidently</Typography>
     </Box>
   )
